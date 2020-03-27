5ba9774485076c28948a96743ce3a45562be4759
fujiwara29 <watasixi@gmail.com>
2020-03-27 19:11:34 +0900

    add paginate japanese

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,16 @@ module Sumuka
     # the framework and any gems in your application.
   end
 end
+
+module Sumuka
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja #追加
+  end
+end
+
+
+
+
